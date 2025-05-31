46f58c0181c5dbb8d5e150ec25defa5d2874762d
Aheyev Alex <aheyevalex@gmail.com>
2025-05-31 06:12:57 +0300

    integrated Transaction model

--- api_calls/call_grok.py
+++ api_calls/call_grok.py
@@ -3,5 +3,5 @@ from openai import OpenAI
 def create_client(XAI_API_KEY):
     return OpenAI(base_url="https://api.x.ai/v1", api_key=XAI_API_KEY)
 
-def generate_image(client: OpenAI, prompt:str ="A cat in a tree"):
+def generate_image(client: OpenAI, prompt: str):
     return client.images.generate(model="grok-2-image-1212", prompt=prompt)
--- main.py
+++ main.py
@@ -1,14 +1,23 @@
 
 from api_calls.call_grok import create_client, generate_image
 from config.static import load_api_key
+from config.models import Transaction
 
 from openai import OpenAI
 
 def main():
+    #Create transaction object
+    transaction = Transaction()
+
+    #Process the prompt to API call
     client = create_client(load_api_key())
-    response = generate_image(client)
-    print(response)
-    print(response.data[0].url)
+    response = generate_image(client, transaction.prompt)
+
+    #Update transaction object
+    transaction.update_result_image_url(response.data[0].url)
+    transaction.update_revised_prompt(response.data[0].revised_prompt)
+    print(transaction.is_complete())
+    print(transaction.__repr__())
 
 if __name__=="__main__":
     main()
